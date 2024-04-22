699703c0d99c298562c5daf67ccbf0abe143bd13
Dmitry <exigo53@gmail.com>
2024-04-22 20:49:12 +0300

    kursovaya oop rework

--- src/storage.py
+++ src/storage.py
@@ -34,33 +34,31 @@ class JSONStorage(AbstractStorage):
         return [Vacancy(**vacancy_data) for vacancy_data in data if self._check_criteria(vacancy_data, criteria)]
 
     def _check_criteria(self, vacancy_data, criteria):
-        def _check_criteria(self, vacancy_data, criteria):
-            # Проверяем ключевое слово в названии
-            if 'keyword' in criteria:
-                keyword = criteria['keyword']
-                if keyword.lower() not in vacancy_data['title'].lower():
-                    return False
+        # Проверяем ключевое слово в названии
+        if 'keyword' in criteria:
+            keyword = criteria['keyword']
+            if keyword.lower() not in vacancy_data['title'].lower():
+                return False
 
-            # Проверяем минимальную требуемую зарплату
-            if 'min_salary' in criteria:
-                min_salary = criteria['min_salary']
-                if vacancy_data['salary'] == 'Зарплата не указана':
-                    return False
-                vacancy_salary = self._parse_salary(vacancy_data['salary'])
-                if vacancy_salary < min_salary:
-                    return False
+        # Проверяем минимальную требуемую зарплату
+        if 'min_salary' in criteria:
+            min_salary = criteria['min_salary']
+            if vacancy_data['salary'] == 'Зарплата не указана':
+                return False
+            vacancy_salary = self._parse_salary(vacancy_data['salary'])
+            if vacancy_salary < min_salary:
+                return False
 
-            return True
+        return True
 
-        def _parse_salary(self, salary):
-            # Пример простого парсинга зарплаты
-            if '-' in salary:
-                min_salary, max_salary = map(int, salary.split('-'))
-                return min_salary
-            elif 'от' in salary:
-                return int(salary[3:])
-            elif 'до' in salary:
-                return int(salary[3:])
-            else:
-                return int(salary)
-        pass
+    def _parse_salary(self, salary):
+        # Пример простого парсинга зарплаты
+        if '-' in salary:
+            min_salary, max_salary = map(int, salary.split('-'))
+            return min_salary
+        elif 'от' in salary:
+            return int(salary[3:])
+        elif 'до' in salary:
+            return int(salary[3:])
+        else:
+            return int(salary)
